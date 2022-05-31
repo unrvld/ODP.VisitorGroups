4d2394210abca4e11b81fb5b19acf6a65c0792f6
David Knipe <davidknipe@users.noreply.github.com>
2022-05-31 17:27:29 +0100

    Work to add a estimated segment count after the description

--- src/UNRVLD.ODP.VisitorGroups/Criteria/Models/AudienciesSelectionFactory.cs
+++ src/UNRVLD.ODP.VisitorGroups/Criteria/Models/AudienciesSelectionFactory.cs
@@ -11,7 +11,10 @@ using System.Collections.Generic;
 using System.Linq;
 using IGraphQLClient = UNRVLD.ODP.VisitorGroups.GraphQL.IGraphQLClient;
 using EPiServer.ServiceLocation;
+using Newtonsoft.Json;
 using UNRVLD.ODP.VisitorGroups.GraphQL.Models;
+using UNRVLD.ODP.VisitorGroups.GraphQL.Models.AudienceCount;
+using Audience = UNRVLD.ODP.VisitorGroups.GraphQL.Models.Audience;
 
 namespace UNRVLD.ODP.VisitorGroups.Criteria.Models
 {
@@ -37,16 +40,76 @@ namespace UNRVLD.ODP.VisitorGroups.Criteria.Models
                           }
                         }";
 
+            var selectItems = new List<SelectListItem>();
+
             try
             {
                 var result = client.Query<AudiencesResponse>(query).Result;
-                return result.Items.Select(a => new SelectListItem {Text = a.Description, Value = a.Name})
-                    .OrderBy(x => x.Text);
+                var orderedResult = result.Items.OrderBy(x => x.Description);
+
+                var allCountsQuery = GetAllCountsQuery(orderedResult.ToList());
+                var allCountsResult = client.Query<dynamic>(allCountsQuery).Result;
+
+
+                foreach (var audience in orderedResult)
+                {
+                    var countObject = JsonConvert.DeserializeObject<AudienceCount>(allCountsResult[audience.Name].ToString());
+                    selectItems.Add(new SelectListItem() {Text = audience.Description + GetCountEstimateString(countObject), Value = audience.Name});
+                }
+
+                return selectItems;
             }
             catch
             {
                 return new List<SelectListItem>();
             }
+        }
+
+        private string GetAllCountsQuery(List<Audience> allAudiences)
+        {
+            var countQuery = $@"query MyQuery {{";
+            foreach (var audience in allAudiences)
+            {
+                countQuery += $@"{audience.Name}: audience(name: ""{audience.Name}"") {{
+                                 population_estimate(percent_error: 10) {{
+                                   estimated_lower_bound
+                                   estimated_upper_bound
+                                 }}
+                               }}
+                               ";
+            }
+            countQuery += $@"}}";
+
+            return countQuery;
+        }
+
+        private string GetCountEstimateString(AudienceCount audienceCount)
+        {
+            if (audienceCount == null ||
+                audienceCount.PopulationEstimate == null)
+            {
+                return string.Empty;
+            }
+
+            if (audienceCount.PopulationEstimate.EstimatedLowerBound == 0)
+            {
+                return " (close to 0 visitors)";
+            }
+
+            if (audienceCount.PopulationEstimate.EstimatedLowerBound == 1)
+            {
+                return " (more than 1 visitor)";
+            }
+
+            if (audienceCount.PopulationEstimate.EstimatedLowerBound < 100)
+            {
+                return $" (more than {audienceCount.PopulationEstimate.EstimatedLowerBound} visitors)";
+            }
+
+            int calc = (audienceCount.PopulationEstimate.EstimatedLowerBound +
+                        audienceCount.PopulationEstimate.EstimatedUpperBound) / 2;
+            return $" (about {calc} visitors)";
+
         }
     }
 }
---
+++ src/UNRVLD.ODP.VisitorGroups/GraphQL/Models/AudienceCount/PopulationEstimate.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace UNRVLD.ODP.VisitorGroups.GraphQL.Models.AudienceCount
+{
+    public class PopulationEstimate
+    {
+        [JsonProperty("estimated_lower_bound")]
+        public int EstimatedLowerBound { get; set; }
+
+        [JsonProperty("estimated_upper_bound")]
+        public int EstimatedUpperBound { get; set; }
+    }
+}
